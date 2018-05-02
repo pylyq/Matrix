5c7e9869c79d95e5fc57a83ce4db6c3b49fc4b7d
matrixhaolin <linhao@matrix.space>
2018-05-02 22:32:39 +0800

    ParallelShapelet codes uploaded

--- ParallelShapelet/ParallelShapelet/src/distanceoncpu.h
+++ ParallelShapelet/ParallelShapelet/src/distanceoncpu.h
@@ -12,7 +12,7 @@
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the Matrix library. If not, see <http://www.gnu.org/licenses/>.
+// along with the Matrix library. If not, see <http://www.gnu.org/licenses/>. 
 #include<stdio.h>
 #include "cuda_runtime.h"
 #include "pub.h"
